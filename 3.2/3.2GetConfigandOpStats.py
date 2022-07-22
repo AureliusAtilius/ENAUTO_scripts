f08f97431883122e064d6c9b3eb5e47b7c20e31a
AureliusAtilius <j.j.h.lindner@gmail.com>
2022-07-21 22:08:09 -0700

    Complete 3.2

--- 3.2/3.2GetConfigandOpStats.py
+++ 3.2/3.2GetConfigandOpStats.py
@@ -1,5 +1,5 @@
 from ncclient import manager
-
+import xmltodict
 router= {
         'host':'ios-xe-mgmt-latest.com',
         'port':'830',
@@ -8,3 +8,31 @@ router= {
 }
 
 
+int_filter = """
+<filter>
+    <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+        <interface>
+            <name>GigabitEthernet2</name>
+        </interface>
+    </interfaces>
+    <interfaces-state xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+        <interface>
+            <name>GigabitEthernet2</name>
+        </interface>
+    <interfaces-state>
+</filter>
+"""
+
+with manager.connect(**router, hostkey_verify=False) as m:
+        netconf_response = m.get(int_filter)
+
+
+python_response= xmltodict.parse(netconf_response.xml)['rpc-reply']['data']
+
+
+int_config= python_response['interfaces']['interface']
+oper_state=python_response['interfaces-state']['interface']
+
+
+print(f"Name: {int_config['name']['#text']}")
+print("")
