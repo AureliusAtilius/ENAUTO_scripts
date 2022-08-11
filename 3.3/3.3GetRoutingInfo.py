f7a481aa9287edc6ce1c25bec92db278e8573fdb
AureliusAtilius <j.j.h.lindner@gmail.com>
2022-08-10 21:55:34 -0700

    Add 3.3GetRoutingInfo.py

---
+++ 3.3/3.3GetRoutingInfo.py
@@ -0,0 +1,18 @@
+import requests,json
+
+router = {
+    'host': "sandbox-iosxe-latest-1.cisco.com",
+    'port': "443",
+    'username': "developer",
+    'password': "C1sco12345"
+}
+
+headers = {
+    'Accept': "application/yang-data+json",
+    'Content-Type': "application/yang-data+json"
+}
+
+base_url = f"https://{router['host']}:{router['port']}/restconf/data/ietf-routing:routing"
+
+response = requests.get(url=base_url, headers=headers, auth=(router['username'], router['password']), verify=False).json()
+print(json.dumps(response, indent=4))
