6c8c0c0d96ae4aaf99b9a829cc67edaf6765e840
AureliusAtilius <j.j.h.lindner@gmail.com>
2022-10-10 20:17:46 -0700

    Create SDWANgetInventory.py

---
+++ 5.2/SDWANgetInventory.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+base_url = "https://sandbox-sdwan-2.cisco.com"
+sess = requests.session()
+
+def get_login(username, password):
+        auth_url = f"{base_url}/j_security_check"
+        login_body = {
+'j_username': username,
+'j_password': password
+}
+        login_resp = sess.post(url=auth_url, data=login_body, verify=False)
+        if login_resp.text:
+                print("***** LOGIN FAILED *****")
+                import sys
+                sys.exit(1)
+        else:
+                print("***** LOGIN SUCCESS *****")
+                return login_resp
+token = get_login('devnetuser', 'RG!_Yw919_83')
+
+def device_output(url):
+        device_urls = {
+        'dev_list': f"{base_url}/dataservice/device",
+        'dev_list_verbose': f"{base_url}/dataservice/system/device/vedges",
+        'dev_health': f"{base_url}/dataservice/monitor",
+        }
+        headers = {
+        'content-type': "application/json"
+        }
+        response = sess.get(url=device_urls[url], headers=headers, verify=False)
+        return(response)
+        
+device_inv = device_output('dev_list').json()
+print(json.dumps(device_inv, indent=2))
