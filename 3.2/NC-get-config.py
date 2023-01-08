a99f5ce18327f87e091b04af1c4996fab31e5e1b
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-01-07 21:07:01 -0800

    Removed argparser.

--- 3.2/NC-get-config.py
+++ 3.2/NC-get-config.py
@@ -1,25 +1,19 @@
 import sys
-from argparse import ArgumentParser
 from ncclient import manager
 import xml.dom.minidom
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description='Select options.')
-    # Input parameters
-    parser.add_argument('--host', type=str, required=True,
-                        help="The device IP or DN")
-    parser.add_argument('-u', '--username', type=str, default='cisco',
-                        help="Go on, guess!")
-    parser.add_argument('-p', '--password', type=str, default='cisco',
-                        help="Yep, this one too! ;-)")
-    parser.add_argument('--port', type=int, default=830,
-                        help="Specify this if you want a non-default port")
-    args = parser.parse_args()
+    router = {
+    'host': "sandbox-iosxe-latest-1.cisco.com",
+    'port': "830",
+    'username': "developer",
+    'password': "C1sco12345"
+}
 
-    m =  manager.connect(host=args.host,
-                         port=args.port,
-                         username=args.username,
-                         password=args.password,
+    m =  manager.connect(host=router['host'],
+                         port=router['port'],
+                         username=router['username'],
+                         password=router['password'],
                          device_params={'name':"csr"})
 
     hostname_filter = '''
