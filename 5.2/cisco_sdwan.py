9dab9f7ada6e0ccddfe113ab6423294634db5bd4
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-02-22 21:18:52 -0800

    Added the following funtions: get_feature_templates add_fd_vsmart_device_templates attach_vsmart_device_template _wait_for_device_action_done _add_policy add_policy_site add_policy_vpn add_policy_sla add_policy_mesh add_policy_approute

--- 5.2/cisco_sdwan.py
+++ 5.2/cisco_sdwan.py
@@ -38,7 +38,7 @@ class CiscoSDWAN:
                         password="Cisco123!",
                 )
         @staticmethod
-        def get_instance_reserverd():
+        def get_instance_reserved():
                 return CiscoSDWAN(
                         host="10.10.20.90", port=8443, username='admin', password='admin'
                 )
@@ -79,3 +79,165 @@ class CiscoSDWAN:
 
                 params = {"model": model} if model else None
                 return self._req("dataservice/system/device/vedges", params=params)
+
+        def get_feature_templates(self):
+                return self._req("dataservice/template/feature")
+
+        def add_fd_vsmart_device_template(self):
+                all_temps= self.get_feature_templates()
+                fd_temps = []
+                for temp in all_temps.json()["data"]:
+
+                        temp_type = temp["templateType"].lower()
+                        if temp["factoryDefault"] and (
+                                temp_type.endswith("vsmart") or temp_type== "aaa"
+                        ):
+                                fd_temps.append(
+                                        {
+                                        "templateId": temp["templateId"],
+                                        "templateType": temp["templateType"],
+                                        }
+                                )
+                body = {
+                        "templateName": "Basic_template",
+                        "templateDescription": "Collection of default templates",
+                        "deviceType": "vsmart",
+                        "configType": "template",
+                        "factoryDefault": False,
+                        "policyId": "",
+                        "featureTemplateUidRange": [],
+                        "generalTemplates": fd_temps,
+                }
+
+                return self._req(
+                        "dataservice/template/device/feature", method="post", jsonbody=body
+                                        
+                )
+
+        def attach_vsmart_device_template(self, template_id, var_map):
+
+                vsmarts = self.get_all_devices(model="vsmart")
+                templates=[]
+
+                for dev in vsmarts.json()["data"]:
+                        site_id, def_gway = var_map[dev["host-name"]]
+                        vsmart_dict= {
+                                "csv-status": "complete",
+                                "csv-deviceId": dev["uuid"],
+                                "csv-deviceIP": dev["system-ip"],
+                                "csv-host-name": dev["host-name"],
+                                "/0/vpn-instance/ip/route/0.0.0.0/0/next-hop/address": def_gway,
+                                "//system/host-name": dev["host-name"],
+                                "//system/site-id": site_id,
+                                "csv-templateId": template_id,
+                        }   
+                        templates.append(vsmart_dict)     
+
+                body= {
+                        "deviceTemplateList": [
+                        {
+                        "templateId": template_id,
+                        "device": templates,
+                        "isEdited": False,
+                        "isMasterEdited": False,
+                        }
+                        ]
+                }
+
+                attach_resp = self._req(
+                        "dataservice/template/device/config/attachfeature",
+                        method="post",
+                        jsonbody=body,
+                )
+
+                attach_id = attach_resp.json()["id"]
+                return self._wait_for_device_action_done(attach_id)
+        
+        def _wait_for_device_action_done(self, uuid, interval=20):
+
+                while True:
+                        time.sleep(interval)
+                        check = self._req(f"dataservice/device/action/status/{uuid}")
+                        if check.json()["summary"]["status"].lower() != "in_progress":
+                                break
+                return check
+
+        def _add_policy(self,obj_type,name,entries):
+
+                body = {
+                        "name": name,
+                        "description": "Desc Not Required",
+                        "type": obj_type,
+                        "entries": entries,
+                }
+                
+                return self._req(
+                        f"dataservice/template/policy/list/{obj_type}",
+                        method="post",
+                        jsonbody=body,
+                )
+        
+        def add_policy_site(self, name, site_list):
+
+                entries= [{"siteId": str(site)} for site in site_list]
+                return self._add_policy("site", name, entries)
+        
+        def add_policy_vpn(self, name, vpn_list):
+
+                entries=[{"vpn": str(vpn)} for vpn in vpn_list]
+                return self._add_policy("vpn", name, entries=entries) 
+        
+        def add_policy_sla(self, name, sla_entries):
+
+                return self._add_policy("sla", name, sla_entries)
+        
+        def add_policy_mesh(self, name, vpn_id, region_map, description="none"):
+
+                regions=[]
+                for region, site_id in region_map.items():
+                        regions.append({"name": region, "siteLists": site_id})
+
+                body = {
+                        "name": name,
+                        "type": "mesh",
+                        "description": description,
+                        "definition": {"vpnList": vpn_id, "regions": regions}
+                }
+
+                return self._req
+        
+        def add_policy_approute(
+                        self, name, sla_id, dscp, pri_link, alt_link, description="none"
+        ):
+                body = {
+                        "name": name,
+                        "type": "appRoute",
+                        "description": description,
+                        "sequences": [
+                                {
+                                "sequenceId": 1,
+                                "sequenceName": "App Route",
+                                "sequenceType": "appRoute",
+                                "match": {"entries":[{"field": "dscp", "value": str(dscp)}]},
+                                "actions": [
+                                        {
+                                                "type": "slaClass",
+                                                "parameter": [
+                                                        {"field": "name", "ref": sla_id},
+                                                        {"field": "preferredColor", "value": pri_link},
+                                                ],
+                                        },
+                                        {
+                                                "type": "backupSlaPreferredColor",
+                                                "parameter": alt_link,
+                                        },
+                                ],
+                                }
+                        ]
+                }
+
+                return self._req(
+                        f"dataservice/template/policy/definition/approute",
+                        method="post",
+                        jsonbody=body,
+                )
