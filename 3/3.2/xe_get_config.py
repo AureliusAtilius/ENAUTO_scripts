c235e21560ac858a08579097be10fba145fd81d5
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-04-05 21:40:51 -0700

    Change script to print Name and IP address of specific loopback interface.

--- 3/3.2/xe_get_config.py
+++ 3/3.2/xe_get_config.py
@@ -1,9 +1,10 @@
 from ncclient import manager
 import xml.dom.minidom
+import xmltodict
 router = {
     'host': "sandbox-iosxe-latest-1.cisco.com",
     'port': "830",
-    'username': "developer",
+    'username': "admin",
     'password': "C1sco12345"
 }
 
@@ -11,7 +12,7 @@ netconf_filter = '''
 <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
     <interfaces xmlns = "urn:ietf:params:xml:ns:yang:ietf-interfaces">
         <interface>
-
+            <name>Loopback55</name>
         </interface>
     </interfaces>
 </filter>
@@ -26,6 +27,11 @@ with manager.connect(
         ) as m:
 
         netconf = m.get_config("running", netconf_filter) 
-        
+
+        python_response= xmltodict.parse(netconf.xml)['rpc-reply']['data']
+        int_config= python_response['interfaces']['interface']
+
+        print(f"Name: {int_config['name']} {int_config['type']['#text']}")
+        print(f"IP Address: {int_config['ipv4']['address']['ip']} Netmask: {int_config['ipv4']['address']['netmask']}")
 
         print(xml.dom.minidom.parseString(netconf.xml).toprettyxml())
