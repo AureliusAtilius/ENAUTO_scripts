702b617e56d17b7c1b7e1a691670d460c8c2ca87
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-04-13 19:26:02 -0700

    fix payload

--- 3/3.5/3.5Configure_Subscription_RESTCONF.py
+++ 3/3.5/3.5Configure_Subscription_RESTCONF.py
@@ -13,14 +13,14 @@ headers = {
       "Content-Type" : "application/yang-data+json"
    }
 
-module = "Cisco-IOS-XE-mdt-cfg:mdt-config-data"
+module = "Cisco-IOS-XE-mdt-cfg:mdt-config-data/mdt-subscription"
 
 url = f"https://{router['host']}:{router['port']}/restconf/data/{module}"
 print(url)
 
 payload = {
-    "mdt-config-data": {
-        "mdt-subscription": {
+        "mdt-subscription": [
+    {
                 "subscription-id": 100,
                 "base": {
                     "stream": "yang-push",
@@ -34,7 +34,28 @@ payload = {
                     "protocol": "grpc-tcp"
                 }
             }
+
         }
+    
+payload = {
+  "mdt-subscription": [
+    {
+      "subscription-id": 100,
+      "base": {
+        "stream": "yang-push",
+        "encoding": "encod-kvgpb",
+        
+        "rcvr-type": "string",
+        "period": 1000,
+        "xpath": "/process-cpu-ios-xe-oper:cpu-usage/cpu-utilization/five-seconds",
+      },
+      "mdt-receivers": {
+        "protocol": "grpc-tcp",
+        "address": "10.10.20.50",
+        "port": 42518
+      }
+    }
+  ]
 }
 
 print(payload)
