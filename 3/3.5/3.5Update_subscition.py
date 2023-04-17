831d147a189d3188a71839b29dfb1aa44feb2821
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-04-16 20:03:31 -0700

    Return configure_subscription to posting sub. Create new file for updating the subscription base.

--- 3/3.5/3.5Update_subscition.py
+++ 3/3.5/3.5Update_subscition.py
@@ -13,29 +13,18 @@ headers = {
       "Content-Type" : "application/yang-data+json"
    }
 
-module = "Cisco-IOS-XE-mdt-cfg:mdt-config-data/mdt-subscription"
+module = "Cisco-IOS-XE-mdt-cfg:mdt-config-data/mdt-subscription=100/base"
 
 url = f"https://{router['host']}:{router['port']}/restconf/data/{module}"
 print(url)
   
-  
 payload = {
-  "mdt-subscription": [
-    {
-      "subscription-id": 100,
-      "base": {
-        "stream": "yang-push",
-        "encoding": "encode-kvgpb",
-        "period": "1000",
-        "xpath": "/process-cpu-ios-xe-oper:cpu-usage/cpu-utilization/five-seconds"
-      },
-      "mdt-receivers": {
-        "protocol": "grpc-tcp",
-        "address": "10.10.20.50",
-        "port": 42518
-      },
-    }
-  ]
+  "base": {
+    "stream": "yang-push",
+    "encoding": "encode-kvgpb",
+    "period": "2000",
+    "xpath": "/process-cpu-ios-xe-oper:cpu-usage/cpu-utilization/five-seconds"
+  }
 }
 print(payload)
 
