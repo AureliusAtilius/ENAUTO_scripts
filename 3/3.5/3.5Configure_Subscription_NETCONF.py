f91ca27d8ec9ae3d468dc2dc824404c06a8c4092
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-04-16 21:03:55 -0700

    update IP

--- 3/3.5/3.5Configure_Subscription_NETCONF.py
+++ 3/3.5/3.5Configure_Subscription_NETCONF.py
@@ -3,7 +3,7 @@ import xmltodict
 from lxml.etree import fromstring
 
 router = {
-    "host":"sandbox-iosxe-latest-1.cisco.com",
+    "host":"10.10.20.48",
     "port":"830",
     "username":"developer",
     "password":"C1sco12345",
