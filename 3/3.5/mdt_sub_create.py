c950676c78f23f0020171087c5ce7d2b571d8395
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-05-02 20:52:02 -0700

    print reason

--- 3/3.5/mdt_sub_create.py
+++ 3/3.5/mdt_sub_create.py
@@ -44,7 +44,8 @@ print("Submitting mdt subscription")
 resp = requests.post(f"https://{r['host']}:443/restconf/data/Cisco-IOS-XE-mdt-cfg:mdt-config-data/",data=json.dumps(payload),headers=headers,auth=(r["username"],r["password"]),verify=False)
 
 print(resp.status_code)
-
+print(resp.reason)
+resp.
 resp = requests.get(f"https://{r['host']}:443/restconf/data/Cisco-IOS-XE-mdt-cfg:mdt-config-data/mdt-subscription",headers=headers,auth=(r["username"],r["password"]),verify=False)
 
 if resp:
