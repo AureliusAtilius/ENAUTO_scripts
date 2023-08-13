1c0e312b52b1785892b2e95bb4345ad306a1d014
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-08-13 13:19:32 -0700

    Add comments

--- 3/3.3/3.3GetNetconfCapabilities.py
+++ 3/3.3/3.3GetNetconfCapabilities.py
@@ -1,5 +1,6 @@
 import requests,json
 
+# dictionary containing device connection info
 router = {
     'host': "sandbox-iosxe-latest-1.cisco.com",
     'port': "443",
@@ -7,15 +8,19 @@ router = {
     'password': "C1sco12345"
 }
 
+# dictionary containing RESTCONF headers
 headers = {
     'Accept': "application/yang-data+json",
     'Content-Type': "application/yang-data+json"
 }
 
+# URL targetting capabilies data
 base_url = f"https://{router['host']}:{router['port']}/restconf/data/netconf-state/capabilities"
 
+# send request for capabilities data
 response = requests.get(url=base_url, headers=headers, auth=(router['username'], router['password']), verify=False)
 
+# if request status code is successful print list of capabilities
 if response.status_code == 200:
     resp_dict = response.json()
     for capability in resp_dict['ietf-netconf-monitoring:capabilities']['capability']:
