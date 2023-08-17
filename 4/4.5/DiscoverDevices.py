2990e3c64343f1228dd35dffe7357bb385d72ca1
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-08-16 21:33:51 -0700

    Add comments.

--- 4/4.5/DiscoverDevices.py
+++ 4/4.5/DiscoverDevices.py
@@ -4,10 +4,16 @@ import time
 import json
 from get_token import get_token
 
+# Script for starting a discovery of network devices.
 
 
+
+# function for obtaining credentials needed to run discovery
 def get_credentials(base_url, headers):
+        # URL for global credential
         cred_url = f"{base_url}/intent/api/v1/global-credential/cli"
+        
+        # dictionary containing credential payload. Substitute your info <here>
         credentials = {
         'description': 'DNA Center Credentials',
         'enablePassword': '<pwd>',
@@ -15,6 +21,8 @@ def get_credentials(base_url, headers):
         'username': '<user>',
         'credentialType': 'GLOBAL'
         }
+
+        # attempt to post credential payload. If successful, response contains a task ID for asynchronous operation. Wait 5 seconds for task to complete
         try:
                 task_id_att = requests.post(url=f"{cred_url}", headers=headers, json=credentials, verify=False)
         except Exception as ex:
@@ -23,8 +31,9 @@ def get_credentials(base_url, headers):
                 task_id = task_id_att.json()
                 task_id_url = f"{base_url}/intent/api/v1/task/{task_id}"
                 time.sleep(5)
+        
+        # attempt to request task response from credential post
         try:
-
                 cred_att = requests.get(url=f"{task_id_url}", headers=headers, verify=False)
         except Exception as ex:
                 print(f"Error has occurred: \n ---> {ex}")
@@ -33,8 +42,13 @@ def get_credentials(base_url, headers):
                 print("*** Credentials acquired ***")
         return creds
 
+
+# function for starting discovery
 def begin_discovery(cred_id):
+        # URL for discovery
         discovery_url = f"{base_url}/intent/api/v1/discovery"
+        
+        # dictionary containing payload with discovery info
         discovery = {
         "name": "Discovery-Guide",
         "discoveryType": "Range",
@@ -45,13 +59,18 @@ def begin_discovery(cred_id):
         "isAutoCdp": False,
         "globalCredentialIdList":cred_id
         }
+
+        # attempt to post the discovery payload to start discovery
         try:
                 task_id_att = requests.post(url=f"{discovery_url}", headers=headers, json=discovery, verify=False)
         except Exception as ex:
                 print(f"Error occurred: \n ---> {ex}")
         else:
+                # wait 10 seconds for task to complete
                 task_id = task_id_att.json()['response']['taskId']
                 time.sleep(10)
+        
+        # attempt to request task response
         try:
                 discovery_att = requests.get(url=f"{task_id}", headers=headers, verify=False)
         except Exception as ex:
@@ -62,20 +81,31 @@ def begin_discovery(cred_id):
 
 
 if __name__=="__main__":
+        
+        # get auth token
         dna_token = get_token()
-        # print(get_token())
+        
+        # REST API headers containing auth token
         headers = {
         "x-auth-token": dna_token,
         "Accept": "application/json",
         "Content-type": "application/json"
         }
-        base_url = "https://sandboxdnac.cisco.com/dna"
-        cred_id = get_credentials(base_url=base_url, headers=headers)
-        disc_id = begin_discovery(cred_id=cred_id)
-        # print(disc_id_url)
 
+        # base URL for DNAC sandbox
+        base_url = "https://sandboxdnac.cisco.com/dna"
+
+        # get credentials
+        cred_id = get_credentials(base_url=base_url, headers=headers)
+
+        # start discovery
+        disc_id = begin_discovery(cred_id=cred_id)
+        
+        # request discovered devices using discovery ID 
         disc_devices_url = f"{base_url}/intent/api/v1/dsicovery/{disc_id}/network-device"
         dev_resp = requests.get(url=f"{disc_devices_url}")
+        
+        # iterate over response data and print list of device IP addresses
         device_ips = []
         for device in dev_resp.json()['response']:
                 device_ips.append({'ip': device['managementIpAddress']})
