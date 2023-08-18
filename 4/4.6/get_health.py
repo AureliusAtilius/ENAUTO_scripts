b73037e3f1faa72c7a1edf973cce0617f09d8280
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-08-17 21:04:36 -0700

    Add comments.

--- 4/4.6/get_health.py
+++ 4/4.6/get_health.py
@@ -5,7 +5,7 @@ from dnac_requester import DNACRequester
 
 def main():
     
-
+    # invoke instance of DNACRequester using DNAC sandbox 
     dnac= DNACRequester(
         host="sandboxdnac.cisco.com",
         username="devnetuser",
@@ -13,23 +13,29 @@ def main():
         verify=False,
         old_style=True
     )
-    print("1")
+    
+    # see if health outputs directory exists in current working directory, if not, create it
     health_dir=f"{Path.cwd()}\\health_outputs"
     if not os.path.exists(health_dir):
         os.makedirs(health_dir)
-    print("2")
-
+    
+    # get current timestamp
     current_epoch = int(time.time() * 1000)
     params = {
         "timestamp": current_epoch
     }
-    print("3")
+    
+    # create a request for each of the three types of health check containing timestamp
     for health in ["network", "site", "client"]:
         health_resp = dnac.req(
             f"dna/intent/api/v1/{health}-health",
             params=params,
         )
+
+        # print response in human friendly format
         print(json.dumps(health_resp.json(),indent=2))
+
+        # write response to a file in the health_outputs folder
         with open(f"{health_dir}\\get_{health}_health.json", "w") as handle:
             json.dump(health_resp.json(),handle,indent=2)
         print(f"Wrote {health} health to disk")
