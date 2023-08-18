bb0d6cd47b0e68e4fd1ab3979574b5fe79256430
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-08-17 20:50:04 -0700

    Add comments.

--- 4/4.6/build_webhook.py
+++ 4/4.6/build_webhook.py
@@ -3,6 +3,7 @@ from dnac_requester import DNACRequester
 
 def main():
     
+    # invoke instance of DNACRequester using DNAC sandbox
     dnac = DNACRequester(
         host = "sandboxdnac.cisco.com",
         username="devnetuser",
@@ -11,12 +12,14 @@ def main():
         old_style=True
     )
 
+    # request and print events
     events= dnac.req(f"dna/intent/api/v1/events", params={"tags": "ASSURANCE"})
-
     print(json.dumps(events.json(),indent=2))
 
+    # get event ID's for each event in response
     event_ids= [event["eventId"] for event in events.json()]
 
+    # json body containing payload for creating event webhook subscription
     body = [
         {
             "name": "globo_assurance",
@@ -35,12 +38,15 @@ def main():
         }
     ]
 
+    # post payload to create subscription
     add_resp= dnac.req(
         f"dna/intent/api/v1/event/subscription", method="post", jsonbody=body
     )
 
+    # wait five seconds
     time.sleep(5)
 
+    # request status of asynchronous operation
     add_status = dnac.req(add_resp.json()["statusUri"][1:]).json()
 
     if add_status["errorMessage"]:
@@ -48,9 +54,10 @@ def main():
     
     print(f"{add_status['statusMessage']} / {add_status['apiStatus']}")
 
+    # request and print list of subscritptions to ensure it was created
     subs_resp = dnac.req(f"dna/intent/api/v1/event/subscription")
 
-    my_sub = subs_resp.json()[-1]
+    print(json.dumps(subs_resp.json(),indent=2))
 
 
 
