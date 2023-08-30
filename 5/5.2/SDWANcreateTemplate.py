34a409a86e1739c7f41aa849c93983783a8af2ac
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-08-29 21:41:32 -0700

    Add comments.

--- 5/5.2/SDWANcreateTemplate.py
+++ 5/5.2/SDWANcreateTemplate.py
@@ -1,9 +1,14 @@
 import requests
 import json
 
+
+# base URL for SDWAN sandbox
 base_url = "https://sandbox-sdwan-2.cisco.com"
+
+# create session object
 sess = requests.session()
 
+# function for requesting authentication token from controller
 def get_login(username, password):
     auth_url = f"{base_url}/j_security_check"
 
@@ -24,6 +29,7 @@ def get_login(username, password):
 
 token = get_login('devnetuser', 'RG!_Yw919_83')
 
+# function for requesting list of templates by type
 def get_templates(url):
     template_urls = {
         't_list': f"{base_url}/dataservice/template/device",
@@ -36,9 +42,11 @@ def get_templates(url):
 
 get_templates('t_list')
 
+# function for creating template
 def create_template():
     create_t_url = f"{base_url}/dataservice/template/device/feature"
 
+    # json payload, include name and description
     payload = {
        'templateName' : "<name>",
         'templateDescription' : "<description>",
@@ -55,21 +63,25 @@ def create_template():
         ]
     }
 
+    # REST API headers
     headers = {
         "Accept" : "application/json",
         "Content-Type" : "application/json"
     }
 
-    response = sess.get(url=create_t_url, headers=headers, payload=payload, verify=False)
+    # post template payload
+    response = sess.post(url=create_t_url, headers=headers, payload=payload, verify=False)
     print(json.dumps(response, indent=2))
 
-# create_template()
+create_template()
 
 template_id = '324543nv-g24a-2b4g-567jh2sd789agb'
 
+# function for attaching device config template
 def attach_template():
     device_temp_url = f"{base_url}/dataservice/template/device/config/input"
 
+    # template payload
     device_template_payload = {
         'templateId': template_id,
         'deviceIds': ["34524724-vabf-g3m7-34c1-4356b6wec"],
@@ -77,10 +89,14 @@ def attach_template():
         'isMasterEdited': False
     }
 
+    # REST API headers
     headers = {
         "Accept" : "application/json",
         "Content-Type" : "application/json"
     }
 
+    # send post with payload
     response = sess.post(url=device_temp_url, headers=headers, data=device_template_payload, verify=False).json()
+
+    # print response in human friendly format
     print(json.dumps(response, indent=2))
