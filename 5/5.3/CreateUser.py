f348f4eab2018d71dd4efe82a2f376d13b76ca2a
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-08-30 21:28:22 -0700

    Add comments.

--- 5/5.3/CreateUser.py
+++ 5/5.3/CreateUser.py
@@ -1,9 +1,8 @@
 from urllib import response
 import requests
 
-#base_url = "https://sandbox-sdwan-2.cisco.com"
-#sess = requests.session()
 
+# function for requesting authentication token from SDWAN controller
 def get_login(username, password):
     auth_url = f"{base_url}/j_security_check"
 
@@ -22,12 +21,13 @@ def get_login(username, password):
         print("***** LOGIN SUCCESS *****")
         return login_resp
 
-#token = get_login('devnetuser', 'RG!_Yw919_83')
 
-
+# function for posting new user account to controller
 def create_user(acct_username, acct_password):
+        # URL targeting user administration on controller
         acct_url=f"{base_url}/dataservice/admin/user"
 
+        # payload containing user account info
         payload={
                 'group':["netadmin"],
                 'description':'Test User200',
@@ -35,36 +35,48 @@ def create_user(acct_username, acct_password):
                 'password':acct_password
         }
 
+        # REST headers
         headers={
                 'Accept':'application/json',
                 'Content-Type':'application/json'
         }
+
+        # post payload request and print response
         response=sess.post(url=acct_url,headers=headers,data=payload,verify=False)
         print(response)
 
-#create_user("test","Pass123!")
 
 
+# function for updating user account info 
 def update_user_pw(acct_username, acct_password):
+        # URL targetting user password
         acct_url=f"{base_url}/dataservice/admin/user/password/test"
 
+        # payload with username and new password
         payload={
                 'userName':acct_username,
                 'password':acct_password
         }
 
+        # REST headers
         headers={
                 'Accept':'applictation/json',
                 'Content-Type':'application/json'
         }
 
+        # post payload request and print response
         response= sess.post(url=acct_url,headers=headers,data=payload,verify=False)
         print(response)
 
 
 if __name__=="__main__":
+        # base URL for SDWAN sandbox controller
         base_url = "https://sandbox-sdwan-2.cisco.com"
+        # create session object
         sess = requests.session()
+        # request authentication for session
         token = get_login('devnetuser', 'RG!_Yw919_83')
+        # create user
         create_user("test","Pass123!")
+        # change user password
         update_user_pw("test","!321ssap")
