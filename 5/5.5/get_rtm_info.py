8ba32ead925faf02ad8aea8c437a771520eb15a5
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-09-06 22:07:45 -0700

    Add comments.

--- 5/5.5/get_rtm_info.py
+++ 5/5.5/get_rtm_info.py
@@ -6,12 +6,14 @@ from cisco_sdwan import CiscoSDWAN
 
 def main():
     
-
+    # invoke CiscoSDWAN instance for always on sandbox
     sdwan = CiscoSDWAN.get_instance_always_on()
 
+    # get list of vEdge devices
     resp = sdwan.get_device_vedges(model="vedge-cloud")
     data_list= resp.json()["data"]
 
+    # iterate over devices in list, get tunnel statistics and print
     for vedge in data_list:
         if vedge.get("configStatusMessage", "").lower() == "in sysc":
             vedge_id= vedge["system-ip"]
