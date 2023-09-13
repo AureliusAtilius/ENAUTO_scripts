d8eca7b69d107c05df51fb0793bfa4bdaf30098a
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-09-12 21:44:03 -0700

    Add comments.

--- 5/5.6/get_dashboard_stats.py
+++ 5/5.6/get_dashboard_stats.py
@@ -5,13 +5,16 @@ from cisco_sdwan import CiscoSDWAN
 
 def main():
     
+    # invoke instance of CiscoSDWAN using always on sandbox
     sdwan = CiscoSDWAN.get_instance_always_on()
 
+    # create list of api calls
     api_calls = [
         sdwan.get_alarm_count,
         sdwan.get_certificate_summary,
         sdwan.get_control_status,
     ]
+    # run api calls
     CiscoSDWAN.run_api_calls(api_calls)
 
 if __name__=="__main__":
