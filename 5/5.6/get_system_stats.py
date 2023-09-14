07334386951a1e66c5b9c541bf6e79515c8f034f
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-09-13 21:49:51 -0700

    Add comments.

--- 5/5.6/get_system_stats.py
+++ 5/5.6/get_system_stats.py
@@ -8,23 +8,27 @@ from cisco_sdwan import CiscoSDWAN
 
 def main():
     
+    # invoke CiscoSDWAN instance using always on sandbox
     sdwan = CiscoSDWAN.get_instance_always_on()
 
-
+    # open json file and load file data
     with open("sys_stat_query.json", "r") as handle:
         query = json.load(handle)
 
+    # use query data from file to get system stats
     sys_stat_resp= sdwan.get_system_stats(query)
 
     outfile= "log_cpumem.csv"
     print(f"Creating '{outfile}' from vManage system stats")
+
+    # create output file and write stat data to file
     with open(outfile, "w") as handle:
         handle.write("dtg,cpu util%,mem util%\n")
         for stat in sys_stat_resp.json() ["data"]:
-        dtg = datetime.fromtimestamp(stat["entry_time"] // 1000, timezone.utc)
-        cpu = round(stat["cpu_user_new"], 2)
-        mem = round(stat["mem_util"], 2)
-        handle.write(f"{dtg},{cpu},{mem}\n")
+            dtg = datetime.fromtimestamp(stat["entry_time"] // 1000, timezone.utc)
+            cpu = round(stat["cpu_user_new"], 2)
+            mem = round(stat["mem_util"], 2)
+            handle.write(f"{dtg},{cpu},{mem}\n")
     print(f"Use 'column -s, -t {outfile} | less -S' to viwe from shell")
 
 if __name__ == "__main__":
